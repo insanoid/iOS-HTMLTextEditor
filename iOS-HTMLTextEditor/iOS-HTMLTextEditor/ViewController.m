8f5a240879d379fe6b9fa060c823824aab695169
Karthikeya Udupa K M <karthikeyaudupa@gmail.com>
2012-08-10 20:21:42 +0530

    Initial Commit.

---
+++ iOS-HTMLTextEditor/iOS-HTMLTextEditor/ViewController.m
@@ -0,0 +1,100 @@
+//
+//  ViewController.m
+//  iOS-HTMLTextEditor
+//
+//  Created by Karthikeya Udupa on 8/10/12.
+//  Copyright (c) 2012 Karthikeya Udupa K M All rights reserved.
+
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self loadContent];
+    
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)loadContent {
+    
+    NSString *path =[[[NSBundle mainBundle] bundlePath] stringByAppendingString:@"/CKEditor/demo.html"];
+    [webView loadRequest:[NSURLRequest requestWithURL:[NSURL fileURLWithPath:path]]];
+    
+}
+
+-(void)webViewDidFinishLoad:(UIWebView *)_webView{
+    
+    [_webView stringByEvaluatingJavaScriptFromString:@"document.getElementById('editor1').innerHTML='<b>Hello</b> <i>World</i>!';"];
+}
+
+- (IBAction)evaluate:(id)sender {
+    
+    
+    NSString *html = [webView stringByEvaluatingJavaScriptFromString: 
+                      @"CKEDITOR.instances.editor1.getData()"];
+    contentLabel.text = html;
+    
+}
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+	[super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+	[super viewDidDisappear:animated];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation==UIInterfaceOrientationPortrait);
+}
+
+@end
